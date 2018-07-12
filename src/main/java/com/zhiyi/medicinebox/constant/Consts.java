82928641d49b7711189b9e54eaeeb80c047cacfc
guanchen <981745954>
2018-07-12 16:01:09 +0800

    add comment

--- src/main/java/com/zhiyi/medicinebox/constant/Consts.java
+++ src/main/java/com/zhiyi/medicinebox/constant/Consts.java
@@ -1,8 +1,11 @@
 package com.zhiyi.medicinebox.constant;
 
 public class Consts {
-	
+
+	/**服药状态 *************************/
+	/** 已服药*/
 	public static final String TAKE_MED_STATUS_EATED = "EATED";
+	/**跳过*/
 	public static final String TAKE_MED_STATUS_SKIP = "SKIP";
 
 }
